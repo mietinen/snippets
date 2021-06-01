942fcaf9f6d0a6266522289830b51c3e4b4e8143
Harry <aleksander@mietinen.net>
2021-06-01 03:21:18 +0200

    Test multiple http(s) connections

---
+++ url-test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Test multiple http/https connections, sort by time to load
+
+urls=(
+	https://www.youtube.com
+	https://www.twitch.com
+	https://www.reddit.com
+)
+
+
+echo "Test multiple http(s) connections, sort by time to load"
+echo
+
+output=""
+for h in "${urls[@]}"; do
+	time=$(curl -Lo /dev/null --connect-timeout 5 -s -w "%{time_connect} %{time_starttransfer} %{time_total}" "$h")
+	if [ $? -eq 0 ] ; then
+		output="${output:+${output}\n}$h $time"
+	else
+		output="${output:+${output}\n}$h FAILED FAILED FAILED"
+	fi
+done
+echo -e "$output" | sort -k4 | column -t -o "    " -N URL,Connect,Start,Total
